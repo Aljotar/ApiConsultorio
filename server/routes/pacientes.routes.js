d6e93f2f96e8977c87d51577cbed17c41eebaebe
Jotar Alvaro <jotaralvaro@gmail.com>
2022-08-11 00:23:25 -0300

    validacion con express validator

--- server/routes/pacientes.routes.js
+++ server/routes/pacientes.routes.js
@@ -1,18 +1,79 @@
+import  express  from 'express';
 import { Router } from "express";
 
 import {createNewPaciente, deletePacienteBydni, getPacienteByDni, getPacientes, updatePacienteBydni} from '../controllers/pacientes.controller.js'
+
+import { check } from 'express-validator'
 
 const router = Router();
 
 router.get('/pacientes', getPacientes)
 
-router.post('/pacientes', createNewPaciente)
+router.post('/pacientes', 
+[
+    check('nombre', 'El nombre es obligatorio')
+        .exists()
+        .not()
+        .isEmpty(),
+    check('apellido', 'El apellido es obligatorio')
+        .exists()
+        .not()
+        .isEmpty(),
+    check('edad')
+        .exists()
+        .not()
+        .isEmpty()
+        .isNumeric(),
+    check('dni','El dni es obligatorio')
+        .exists()
+        .not()
+        .isEmpty()
+        .isNumeric(),
+    check('telefono')
+        .not()
+        .isEmpty()
+        .isNumeric(),
+    check('email')
+        .not()
+        .isEmpty()
+        .isEmail()
+]
+, createNewPaciente)
 
 router.get('/pacientes/:dni', getPacienteByDni)
 
 router.delete('/pacientes/:dni', deletePacienteBydni)
 
-router.put('/pacientes', updatePacienteBydni)
+router.put('/pacientes',
+[
+    check('nombre', 'El nombre es obligatorio')
+        .exists()
+        .not()
+        .isEmpty(),
+    check('apellido', 'El apellido es obligatorio')
+        .exists()
+        .not()
+        .isEmpty(),
+    check('edad')
+        .exists()
+        .not()
+        .isEmpty()
+        .isNumeric(),
+    check('dni','El dni es obligatorio')
+        .exists()
+        .not()
+        .isEmpty()
+        .isNumeric(),
+    check('telefono')
+        .not()
+        .isEmpty()
+        .isNumeric(),
+    check('email')
+        .not()
+        .isEmpty()
+        .isEmail()
+] 
+,updatePacienteBydni)
 
 
 
--- server/routes/turnoPaciente.routes.js
+++ server/routes/turnoPaciente.routes.js
@@ -1,18 +1,81 @@
+import express from 'express';
 import { Router } from "express";
 import { createNewTurno, getTurnoByDni, getTurnos, updateTurnoBydni } from "../controllers/turnos.controller.js";
+
+import { check } from 'express-validator'
 
 const router = Router();
 
 router.get('/turnos', getTurnos)
 
-router.post('/turnos', createNewTurno)
+router.post('/turnos',
+    [
+        check('dni', 'El nombre es obligatorio')
+            .exists()
+            .not()
+            .isEmpty()
+            .isNumeric(),
+        check('apellido', 'El apellido es obligatorio')
+            .exists()
+            .not()
+            .isEmpty(),
+        check('nombre')
+            .exists()
+            .not()
+            .isEmpty(),
+        check('email')
+            .not()
+            .isEmpty()
+            .isEmail(),
+        check('telefono')
+            .not()
+            .isEmpty()
+            .isNumeric(),
+        check('fechaTurno')
+            .not()
+            .isEmpty(),
+        check('estado')
+            .not()
+            .isEmpty()
+            .isNumeric(),
+        check('observacion')
+            .not()
+            .isEmpty(),
+        check('idMedico')
+            .not()
+            .isEmpty()
+    ]
+    , createNewTurno)
 
-router.get('/turnos/:dni', getTurnoByDni)
+router.get('/turnos/:dni',
+    [
+        check('dni', 'El nombre es obligatorio')
+            .exists()
+            .not()
+            .isEmpty()
+            .isNumeric()
+    ]
+    , getTurnoByDni)
 
-router.delete('/turnos/:dni', )
+router.delete('/turnos/:dni',)
 
-router.put('/turnos', updateTurnoBydni)
-
-
+router.put('/turnos',
+    [
+        check('dni', 'El nombre es obligatorio')
+            .exists()
+            .not()
+            .isEmpty()
+            .isNumeric(),
+        check('estado')
+            .exists()
+            .not()
+            .isEmpty()
+            .isNumeric(),
+        check('observacion')
+            .exists()
+            .not()
+            .isEmpty()
+    ]
+    , updateTurnoBydni)
 
 export default router
